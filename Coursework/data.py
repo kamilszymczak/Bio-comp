0ec6537b86890ecb1780c4202c57833cd100b3b9
Sam Fay-Hunt <samfayhunt@gmail.com>
2020-10-05 19:01:08 +0100

    Some small changes to data

--- Coursework/data.py
+++ Coursework/data.py
@@ -1,10 +1,21 @@
 import pandas as pd
 
 class Data:
+    """Data class to handle normalization and structure of input data
+    """
 
     # TODO: specify number of features or detect automatically? 
-    def __init__(self, infile, normalize=False) -> None:
-        self.df = pd.read_csv(infile, header=None, delimiter="\t", na_values='.') #read the data
+    def __init__(self, infile, normalize=False, delim="\t") -> None:
+        """Constructor for Data class
+
+        :param infile: String of filename
+        :type infile: String
+        :param normalize: To normalize this data, defaults to False
+        :type normalize: bool, optional
+        :param delim: Delimiter for reading data
+        :type delim: String
+        """
+        self.df = pd.read_csv(infile, delimiter=delim, na_values='.', header='infer') #read the data
 
         if normalize:
             self.normalize()
@@ -14,11 +25,21 @@ class Data:
         self.df = (self.df-self.df.min()) / (self.df.max()-self.df.min())
 
     # returns all features except the outcome as a numpy matrix
-    def getX(self):
+    def get_rows(self):
+        """Get all the rows of the datafile, excluding the outcome column
+
+        :return: returns numpy ndarray of input data
+        :rtype: ndarray
+        """
         return self.df.iloc[:,:-1].to_numpy()
 
     # returns outcome column as a numpy matrix
-    def getY(self):
+    def get_output(self):
+        """Desired result vector
+
+        :return: numpy ndarray with the actual results
+        :rtype: ndarray
+        """
         return self.df.iloc[:,-1].to_numpy()
 
 

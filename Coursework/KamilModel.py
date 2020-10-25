5019443b7de02e2faec31dfde29fcf7cb17ab40f
Kamil <kamil_szymczak@hotmail.com>
2020-10-25 21:56:40 +0000

    Kamil's: Comments + sigmoid method + exception checks in forward propagation method

--- Coursework/KamilModel.py
+++ Coursework/KamilModel.py
@@ -1,6 +1,16 @@
 import numpy as np
 import inspect
 
+def sigmoid(z):
+    """Applies sigmoid activation function
+
+    :param z: The matrix of weighted sum values for each neuron
+    :type z: numpy.ndarray
+    :return: The matrix with sigmoid applied to each element
+    :rtype: numpy.ndarray
+    """
+    return 1.0/(1.0 +np.exp(-z))
+
 def __weight_matrix__(x, y):
         """Return a numpy array of random weights with the specified size
 
@@ -13,11 +23,29 @@ def __weight_matrix__(x, y):
 
 
 def __single_layer_forward_propagation__(N_prev, W_curr, bias, activation):
+    """Return a numpy array of layer's output
+
+    :param N_prev: matrix of previous layer's output
+    :type N_prev: numpy.ndarray
+    :param W_curr: matrix of weights of current layer
+    :type y: numpy.ndarray
+    :param bias: matrix of previous layer's output
+    :type bias: numpy.ndarray
+    :param activation: activation function to use
+    :type activation: string
+    """
+
+    if not (N_prev.shape[1] == W_curr.shape[0]):
+        raise Exception("The number of columns in the input_matrix must equal the number of rows in the weight_matrix.")
+    
+    if not (bias.shape[0] == W_curr.shape[1]):
+        raise Exception("The number of elements in the bias vector should be the same as the number of columns in the weigth matrix.")
+
     Z_curr = np.dot(N_prev, W_curr)
     Z_curr = Z_curr + bias
 
     # TODO: add more activation functions
-    if activation is "sigmoid":
+    if activation == "sigmoid":
         activation_fun = sigmoid
 
     return activation_fun(Z_curr)
@@ -31,8 +59,6 @@ class ANN:
         """
         # List of layers
         self.layers=[]
-        self.col_int=None
-        self.input=None
         self.compiled=False
 
     def add(self, layer):
@@ -41,29 +67,28 @@ class ANN:
         :param layer: Add an instance of the layer class
         :type layer: Layer
         """
-        if self.input == None:
+        if len(self.layers) < 1:
             raise Exception("Define input first before creating layers.")
         if not inspect.isclass(Layer):
             raise Exception("Parameter must be an instance of the Layer class.")
         self.layers.append(layer)
 
 
-    def set_input(self, input_matrix):
+    def set_input(self, input_matrix, use_bias=True):
         """Insert input matrix and Set the number of columns in the input matrix for the ANN
 
         :param input_matrix: matrix of input data
-        :type col_int: int
+        :type imput_matrix: numpy.ndarray
+        :param use_bias: If to use bias or not
+        :type use_bias: boolean
         """
-        # self.input_cols = input_matrix.shape[1]
-        self.layers.append(Layer(input_matrix.shape[1], use_bias=True))
-        self.input = input_matrix
+        self.layers.append(Layer(input_matrix.shape[1], use_bias))
+        self.layers[0].neurons_val = input_matrix
 
     def compile(self):
         """Compile the Neural Network so it is ready for training or inference
         """
-        if self.input_cols == None:
-            raise Exception("Must define the size of the input matrix.")
-        if len(self.layers) < 2:
+        if len(self.layers) < 3:
             raise Exception("The ANN needs at least 1 hidden and 1 output layer.")
         self.__generate_weights__()
         self.compiled = True
@@ -77,63 +102,55 @@ class ANN:
         :raises Exception: Not Compiled exception
         :raises Exception: Misconfigured input columns exception
         """
+        # TODO: if we want super good error checking set compiled to false every time adding new layer e.g. adding layer after compiling
         if not self.compiled:
             raise Exception("The neural network must be compiled before performing training or inference.")
-        if not input_matrix.shape[1] == self.weights[0]:
-            raise Exception("The input columns have been misconfigured. Expected: ", self.input_cols, "Actual: ", input_matrix.shape[1])
 
-        # First layer / input and first set of weights (OLD)
-        # self.layer_outputs = [__single_layer_forward_propagation__(self.input, self.weights[0], self.bias[0], self.layers[0].activation)]
-        # for i in range(1, len(self.weights)):
-        #     self.layer_outputs = self.layer_outputs + [__single_layer_forward_propagation__(self.layer_outputs[i-1], self.weights[i], self.bias[i], self.layers[i].activation)]
 
-        # NEW
         for i in range(1, len(self.layers)):
-            if i == len(self.layers)-1:
-                output = __single_layer_forward_propagation__(self.layer[i-1].neurons_val, self.layer[i-1].weights, self.layer[i-1].bias, self.layers[i-1].activation)
-                output_layer = Layer(output.shape[1])
-                output_layer.neurons_val = output
-                break
+            self.layers[i].neurons_val = __single_layer_forward_propagation__(self.layers[i-1].neurons_val, self.layers[i].weights, self.layers[i-1].bias, self.layers[i].activation)
 
-            self.layer[i].neurons_val = __single_layer_forward_propagation__(self.layer[i-1].neurons_val, self.layer[i-1].weights, self.layer[i-1].bias, self.layers[i-1].activation)
+        return self.layers[-1]
 
-        return output_layer
 
     def __generate_weights__(self):
         """Generates the weight matrices & bias vectors for the ANN Layers
         """
-        neuron_definition = [self.input_cols]
-        temp_bias = []
-        
-        # Build the bias list & collect the number of neurons into a single list
-        for i in range(len(self.layers)):
-            temp_bias = temp_bias + [2*np.random.rand(self.layers[i].neurons)-1]
-            neuron_definition = neuron_definition + [self.layers[i].neurons]
-        
-        # construct the weight matrices
-        # neuron_definition[0] is the number of columns of the input layer
-        temp_weights = []
-        for i  in range(1, len(neuron_definition)):
-            temp_weights = temp_weights + [__weight_matrix__(neuron_definition[i-1], neuron_definition[i])]
-        
-        self.weights = temp_weights
-        self.bias = temp_bias
-
-        for i in range(len(self.layers)):
-            self.layers[i].weigths = __weight_matrix__(neuron_definition[i-1], neuron_definition[i])
+        #Build bias for every layer except output
+        for i in range(len(self.layers)-1):
             if self.layers[i].use_bias:
-                self.layers[i].bias = 2*np.random.rand(self.layers[i].neurons)-1
+                self.layers[i].bias = 2*np.random.rand(self.layers[i+1].neurons)-1
+
+        #Build weight matrix for every hidden layer
+        for i in range(1, len(self.layers)):
+            self.layers[i].weights = __weight_matrix__(self.layers[i-1].neurons, self.layers[i].neurons)
+
+        # #TODO: Below should do both above in a single loop (but less readable)
+        # #Build bias for every layer except output
+        # for i in range(len(self.layers)-1):
+        #     if self.layers[i].use_bias:
+        #         self.layers[i].bias = [2*np.random.rand(self.layers[i].neurons)-1]
+            
+        #     self.layers[i+1].weights = __weight_matrix__(self.layers[i].neurons, self.layers[i+1].neurons)
+
 
 class Layer:
     """Layer class used to add layers to the ANN
     """
     def __init__(self, neurons, activation=None, use_bias=True):
         """Construct a new Layer
+
+        :param neurons: Number of neurons the layer will contain
+        :type neurons: int
+        :param activation: Activation function the layer will use, if any
+        :type neurons: string
+        :param use_bias: If to use bias or not
+        :type use_bias: boolean
         """
         self.neurons = neurons
         self.neurons_val = []
-        self.weights = []
-        self.bias = []
+        self.weights = None
+        self.bias = None
         self.activation = activation
         self.use_bias = use_bias
 

68bb98d463c7f4a7ff99ea002b2fbe94cb4d65e0
Sam Fay-Hunt <samfayhunt@gmail.com>
2020-11-13 00:46:30 +0000

    Split pswarm and psobehaviour

---
+++ Coursework/PSO/psobehaviour.py
@@ -0,0 +1,135 @@
+from functools import total_ordering
+import numpy as np
+from datetime import datetime, timedelta
+import enum
+
+
+BoundaryPolicy = enum.Enum(
+    'BoundaryPolicy', 'RANDOMREINIT REFUSE BOUNCE')
+
+
+TerminationPolicy = enum.Enum(
+    'TerminationPolicy', 'ITERATIONS DURATION CONVERGENCE')
+
+@total_ordering
+class FitnessLoc:
+    """A comparable class to store a fitness value and vector (position)
+
+        :param location: A vector representing a particles location (or the parameters being optimised)
+        :type location: numpy.array
+        :param fitness: A value indicating the fitness of the location
+        :type fitness: float
+    """
+
+    def __init__(self, location, fitness):
+        self.location = location
+        self.fitness = fitness
+
+    def _is_valid_operand(self, other):
+        return(hasattr(other, "fitness") and hasattr(other, "location"))
+
+    def __eq__(self, other):
+        if not self._is_valid_operand(other):
+            return NotImplemented
+        return self.fitness == other.fitness
+
+    def __lt__(self, other):
+        if not self._is_valid_operand(other):
+            return NotImplemented
+        return self.fitness < other.fitness
+
+
+class TerminationPolicyManager:
+    """An iterator to control the PSO termination behaviour
+
+        :param termination_policy: Sets the policy PSO will use to terminate, can be a list of policies
+        :type termination_policy: TerminationPolicy or list(TerminationPolicy)
+        :param max_iter: The maximum number of iterations to run PSO, defaults to None
+        :type max_iter: int, optional
+        :param min_fitness_delta: The smallest fitness change to look for, defaults to None
+        :type min_fitness_delta: float/int, optional
+        :param time_delta: The time delta object from datetime, defaults to None
+        :type time_delta: datetime.timedelta, optional
+        :raises ValueError: Max iteration undefined
+        :raises ValueError: Time delta undefined
+        :raises ValueError: Fitness delta undefined
+    """
+
+    def __init__(self, termination_policy, max_iter=None, min_fitness_delta=None, time_delta=None):
+        if type(termination_policy) is not list:
+            self.termination_policy = [termination_policy]
+        else:
+            self.termination_policy = termination_policy
+
+        if TerminationPolicy.ITERATIONS in self.termination_policy:
+            if max_iter is None:
+                raise ValueError('Max iteration undefined')
+
+        if TerminationPolicy.DURATION in self.termination_policy:
+            if time_delta is None:
+                raise ValueError('Time delta undefined')
+
+        if TerminationPolicy.CONVERGENCE in self.termination_policy:
+            if min_fitness_delta is None:
+                raise ValueError('Fitness delta undefined')
+
+        # Loop until terminate is true
+        self.terminate = False
+
+        # Number of iterations so far
+        self.current_iter = 0
+        self.max_iter = max_iter
+
+        # The fitness from the last iteration
+        self.min_fitness_delta = min_fitness_delta
+        self.current_fitness_delta = None
+        self.got_fitness_delta = False
+
+        self.start_time = datetime.now()
+        self.time_delta = time_delta
+
+    def next_iteration(self, fitness_delta=None):
+        """Step the termination policy manager forward
+
+        :param fitness_delta: a value representing the delta beween the last best fitness and the current, defaults to None
+        :type fitness_delta: float, optional
+        :raises ValueError: Fitness delta unspecified - when TerminationPolicy.CONVERGENCE is used and fitness delta has not been updated this iteration
+        :raises StopIteration: When the termination condition has been satisfied
+        """
+        if TerminationPolicy.ITERATIONS in self.termination_policy:
+            if self.max_iter < self.current_iter:
+                self.terminate = True
+
+        if TerminationPolicy.DURATION in self.termination_policy:
+            elapsed = datetime.now() - self.start_time
+            if elapsed > self.time_delta:
+                self.terminate = True
+
+        if TerminationPolicy.CONVERGENCE in self.termination_policy:
+            if fitness_delta is not None and self.min_fitness_delta >= fitness_delta:
+                self.terminate = True
+            elif self.got_fitness_delta:
+                self.got_fitness_delta = False
+                if self.min_fitness_delta >= self.current_fitness_delta:
+                    self.terminate = True
+            else:
+                raise ValueError('Fitness delta unspecified')
+
+        self.current_iter += 1
+
+    def update_fitness_delta(self, fitness_delta):
+        """Update the fitness delta to a new value
+
+        :param fitness_delta: the current fitness value
+        :type fitness_delta: 
+        """
+        self.current_fitness_delta = fitness_delta
+        self.got_fitness_delta = True
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        self.next_iteration()
+        if self.terminate is True:
+            raise StopIteration
--- Coursework/PSO/pswarm.py
+++ Coursework/PSO/pswarm.py
@@ -1,34 +1,10 @@
-from functools import total_ordering
 import numpy as np
-from datetime import datetime, timedelta
-import enum
 import random
 
-# Need a vector of all weights in a decodable order in the network
-# Need a vector of all activation functions in the network
-
-# Need to define the boundaries (-1, 1)
-
-
-
-# need to define the behaviour when you hit a boundary (random re-intit, bounce, reject dimensions out of bounds)
-
-# define movement behaviour
-
-# information sharing 
-
-# sub boundary groups: define the inner boundaries for subsets of particles
-
-
-TerminationPolicy = enum.Enum(
-    'TerminationPolicy', 'ITERATIONS DURATION CONVERGENCE')
-
-BoundaryPolicy = enum.Enum(
-    'BoundaryPolicy', 'RANDOMREINIT REFUSE BOUNCE')
+from .psobehaviour import FitnessLoc, TerminationPolicyManager, TerminationPolicy
 
 class PSO:
-    def __init__(self, swarm_size=10, bound=(1, -1), alpha=0.1, beta=1.3, gamma=1.4, delta=1.3, epsilon=0.1, max_iter=int(1e6)):
-        """PSO constructor
+    """Particle Swarm Optimiser
 
         :param swarm_size: desired swarm size, defaults to 10
         :type swarm_size: int, optional
@@ -47,6 +23,7 @@ class PSO:
         :param max_iter: The maximum number of iterations before termination, defaults to 0.1
         :type max_iter: int, optional
         """
+    def __init__(self, swarm_size=10, bound=(1, -1), alpha=0.1, beta=1.3, gamma=1.4, delta=1.3, epsilon=0.1, max_iter=int(1e6)):
         self.swarm_size = swarm_size
         self.boundary = bound
         self.alpha = alpha
@@ -94,12 +71,16 @@ class PSO:
 
 
     def run(self):
+        """Begin Particle Swarm Optimisation - Search dimensions must have been specified
+        """
+        if not self.search_dimension_set:
+            raise ValueError('Search dimentions have not yet been specified')
         self._instantiate_particles()
         self.best = None
-        previous_best = self.best
-        controller = IterationController(TerminationPolicy.ITERATIONS, max_iter=1000)
 
-        while controller.terminate:
+        controller = TerminationPolicyManager(TerminationPolicy.ITERATIONS, max_iter=1000)
+
+        while not controller.terminate:
             # Update best and personal fitness values based on the current positions
             # only iterate through particles that will move, thus continue if all velocities of a given particle are 0
             self._assess_fitness()
@@ -110,7 +91,7 @@ class PSO:
             # Move the particles based on their velocity
             self._move_particles()
 
-            controller.next_iteration(self.best)
+            controller.next_iteration(fitness_delta=self.best-self.previous_best)
 
 
 
@@ -127,8 +108,8 @@ class PSO:
             particle.assess_fitness(self.fitness_fn)
 
             if self.best is None or particle.fitness > self.best:
+                self.previous_best = self.best
                 self.best = FitnessLoc(particle.position, particle.fitness)
-
 
 
     def _update_particle(self):
@@ -165,6 +146,13 @@ class PSO:
 
 
 class Particle:
+    """A particle within a PSO optimiser
+
+        :param position: The start position of the Particle
+        :type position: numpy.array
+        :param velocity: The initial velocity of the particle, defaults to None
+        :type velocity: numpy.array, optional
+    """
     def __init__(self, position, velocity = None):
         self.position = position
         self.velocity = velocity
@@ -187,91 +175,3 @@ class Particle:
             self.personal_fittest_loc = FitnessLoc(self.position, self.fitness)
 
 
-
-@total_ordering
-class FitnessLoc:
-    """A comparable class to store a fitness value and vector (position)
-    """
-    def __init__(self, location, fitness):
-        self.location = location
-        self.fitness = fitness   
-
-    def _is_valid_operand(self, other):
-        return(hasattr(other, "fitness") and hasattr(other, "location"))
-
-    def __eq__(self, other):
-        if not self._is_valid_operand(other):
-            return NotImplemented
-        return self.fitness == other.fitness
-
-    def __lt__(self, other):
-        if not self._is_valid_operand(other):
-            return NotImplemented
-        return self.fitness < other.fitness
-
-class IterationController:
-    def __init__(self, termination_policy, max_iter=None, min_fitness_delta=None, time_delta=None):
-
-        if type(termination_policy) is not list:
-            self.termination_policy = [termination_policy]
-        else:
-            self.termination_policy = termination_policy
-        
-        if TerminationPolicy.ITERATIONS in self.termination_policy:
-            if max_iter is None:
-                raise ValueError("Max iteration undefined")
-
-        if TerminationPolicy.DURATION in self.termination_policy:
-            if time_delta is None:
-                raise ValueError("Time delta undefined")
-
-        if TerminationPolicy.CONVERGENCE in self.termination_policy:
-            if min_fitness_delta is None:
-                raise ValueError("Fitness delta undefined")
-        
-        # Loop until terminate is true
-        self.terminate = False
-
-        # Number of iterations so far
-        self.current_iter = 0
-        self.max_iter = max_iter
-
-        # The fitness from the last iteration
-        self.min_fitness_delta = min_fitness_delta
-        self.current_fitness_delta = None
-        self.got_fitness_delta = False
-
-        self.start_time = datetime.now()
-        self.time_delta = time_delta
-
-
-    def next_iteration(self, fitness_delta=None):
-
-        if TerminationPolicy.ITERATIONS in self.termination_policy:
-            if self.max_iter < self.current_iter:
-                self.terminate = True
-
-        if TerminationPolicy.DURATION in self.termination_policy:
-            elapsed = datetime.now() - self.start_time
-            if elapsed > self.time_delta:
-                self.terminate = True
-
-        if TerminationPolicy.CONVERGENCE in self.termination_policy:
-            if fitness_delta is not None and self.min_fitness_delta >= fitness_delta:
-                self.terminate = True
-            elif self.got_fitness_delta:
-                self.got_fitness_delta = False
-                if self.min_fitness_delta >= self.current_fitness_delta:
-                    self.terminate = True
-            else: 
-                raise ValueError('Fitness delta unspecified')
-
-        self.current_iter += 1
-
-        def __iter__(self):
-            return self
-
-        def __next__(self):
-            self.next_iteration()
-            if self.terminate is True:
-               raise StopIteration 

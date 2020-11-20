f67ea1d1502469a514f6c0347dea7a1d83174596
Sam Fay-Hunt <samfayhunt@gmail.com>
2020-11-20 22:36:47 +0000

    Added interface doc strings

--- Coursework/PSO/interface.py
+++ Coursework/PSO/interface.py
@@ -4,12 +4,24 @@ from abc import ABC, abstractmethod
 class Optimisable(ABC):
     @abstractmethod
     def evaluate_fitness(self, vec):
+        """Evaluate the fitness of self given the parameters as a vector
+
+        :param vec: A vector describing a subset of the parameters being searched
+        :type vec: list(float)
+        """
         pass
 
     @abstractmethod
     def dimension_vec(self):
+        """A method to return a vector that defines the search space
+        """
         pass
 
     @abstractmethod
     def decode_vec(self, vec):
+        """A convienience method for evaluate_fitness
+
+        :param vec: A vector describing a subset of the parameters being searched
+        :type vec: list(float)
+        """
         pass

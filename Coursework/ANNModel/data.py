85411d632f279036ed4ed56a35ebf22717b2c1ce
Kamil <kamil_szymczak@hotmail.com>
2020-11-21 18:05:30 +0000

    Data class accuracy score function

--- Coursework/ANNModel/data.py
+++ Coursework/ANNModel/data.py
@@ -30,7 +30,7 @@ class Data:
         """Get all the rows of the datafile, excluding the outcome column
 
         :return: returns numpy ndarray of input data
-        :rtype: ndarray
+        :rtype: numpy.array
         """
         return self.df.drop(self.df.columns[-1],axis=1).to_numpy()
 
@@ -39,10 +39,45 @@ class Data:
         """Desired result vector
 
         :return: numpy ndarray with the actual results
-        :rtype: ndarray
+        :rtype: numpy.array
         """
         return self.df.iloc[:,-1].to_numpy()
 
+    def score(self, model, test_labels, data_name):
+        """Accuracy Scores of model
 
+        :param model: ANN model 
+        :type model: ANNModel.Model.ANN
+        :param test_labels: actual test data outcomes
+        :type test_labels: numpy.array
+        :param data_name: Name of dataset
+        :type data_name: string
+        :return: tuple of scores 
+        :rtype: tuple(float)
+        """
+        correctly_classified_A, correctly_classified_B = 0, 0
 
-    
+        decimal_palces = {
+            'cubic': 4,
+            'linear': 2,
+            'sine': 4,
+            'tanh': 4,
+            'complex': 4,
+            'xor': 0
+        }
+
+        #get predictions
+        predictions = np.round(model.y_hat, decimal_palces.get(data_name))
+        # print(model.y)
+        # print(predictions)
+
+        #loop through predictions
+        for index, pred in enumerate(predictions):
+
+            if np.isclose(test_labels[index], pred):
+                correctly_classified_A += 1
+
+            if np.isclose(pred, test_labels[index]):
+                correctly_classified_B += 1
+
+        return correctly_classified_A/len(test_labels), correctly_classified_B/len(test_labels)     
